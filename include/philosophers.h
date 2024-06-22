cdde83425655f397505bb3845a9fc43005e3a230
Carolina Papes <capapes@cbr11s5.42barcelona.com>
2024-06-22 18:57:35 +0200

    Add vs code ignore and philosophers header

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,13 +6,13 @@
 /*   By: capapes <capapes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 16:51:17 by capapes           #+#    #+#             */
-/*   Updated: 2024/06/22 16:51:35 by capapes          ###   ########.fr       */
+/*   Updated: 2024/06/22 18:53:01 by capapes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-# include <stdio.h>
+int	ft_are_str_int(int argc, char *argv[]);
 
 # endif
