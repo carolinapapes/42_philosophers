8fcf680717b15ee70360ae07b40725da9d858cd6
Carolina Papes <capapes@car8s2.42barcelona.com>
2024-07-05 23:21:55 +0200

    clean exit from create thread error

--- src/philo__rutine.c
+++ src/philo__rutine.c
@@ -6,7 +6,7 @@
 /*   By: capapes <capapes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 19:56:07 by carolinapap       #+#    #+#             */
-/*   Updated: 2024/07/04 19:33:18 by capapes          ###   ########.fr       */
+/*   Updated: 2024/07/05 23:20:14 by capapes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,15 @@ static void	think(t_program *program, int *i, int index)
 	philo__write(program, i, index, "is thinking");
 }
 
-static void	trigger_run(t_program *program, int index)
+static int	trigger_run(t_program *program, int index)
 {
 	pthread_mutex_lock(&program->mx_start);
+	if (program->philos_end)
+		return (pthread_mutex_unlock(&program->mx_start), 1);
 	pthread_mutex_unlock(&program->mx_start);
 	if (!(index & 1))
 		usleep(program->time_to_eat - 100);
+	return (0);
 }
 
 void	philo__rutine(t_philo *philo)
@@ -46,7 +49,8 @@ void	philo__rutine(t_philo *philo)
 	c = &i;
 	index = philo->index;
 	program = philo->program;
-	trigger_run(program, index);
+	if (trigger_run(program, index))
+		return ;
 	while (i)
 	{
 		philo__eat(philo, c, program, index);
